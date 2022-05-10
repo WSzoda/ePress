5932d5c9e179b39d9502a89316127de7fbb95144
Wojciech Szoda <wojciechszoda@icloud.com>
2022-05-10 11:28:59 +0200

    changes in main java file, Added dialog popup to authors scene

--- src/main/java/com/example/projekt/Main.java
+++ src/main/java/com/example/projekt/Main.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-page.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("authors-main-scene.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
---
+++ src/main/java/com/example/projekt/controllers/AuthorsController.java
@@ -0,0 +1,53 @@
+package com.example.projekt.controllers;
+
+import com.example.projekt.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextInputDialog;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Stack;
+
+public class AuthorsController {
+
+    @FXML
+    private ListView authorsList;
+
+
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private void backScene(ActionEvent event) throws IOException {
+        root = new FXMLLoader(Main.class.getResource("programowy-page.fxml")).load();
+        scene = new Scene(root);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    private void handleAddNewAuthor(ActionEvent event){
+        TextInputDialog textInput = new TextInputDialog();
+
+        textInput.setTitle("Add new Author");
+
+        textInput.getDialogPane().setContentText("Name");
+
+        Optional<String> result = textInput.showAndWait();
+
+        if(result.isPresent()){
+            authorsList.getItems().add(result);
+        }
+
+        System.out.println("Dziala");
+    }
+}
