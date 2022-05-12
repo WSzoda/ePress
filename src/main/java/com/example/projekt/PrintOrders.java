e87dd8ec6f7f8289d09aeb66d57dffac883bce52
Wojciech Szoda <wojciechszoda@icloud.com>
2022-05-12 11:24:25 +0200

    PrintOrders implementation

--- src/main/java/com/example/projekt/PrintOrders.java
+++ src/main/java/com/example/projekt/PrintOrders.java
@@ -5,6 +5,44 @@ import java.util.List;
 public class PrintOrders {
     List<PrintOrder> printOrders;
 
+    public void fullFillOrder(int id){}
+    public List<PrintOrder> loadOrdersFromFile(){}
+    private int getNextId(){}
+    private void deleteOrder(){}
+    private void addAuthor(){}
+
+
+
     public static class PrintOrder {
+        private int id;
+        private int bookId;
+        private int amount;
+
+        public PrintOrder(int id, int bookId, int amount) {
+            this.id = id;
+            this.bookId = bookId;
+            this.amount = amount;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public int getBookId() {
+            return bookId;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public String prepareToSaveToFile(){
+            return String.format("%d;%s;%s;%s", id, bookId, amount);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Order Id:%s | Book Id %s | Amount: %s", id, bookId, amount)
+        }
     }
 }
--- src/main/java/com/example/projekt/Printers.java
+++ src/main/java/com/example/projekt/Printers.java
@@ -1,6 +1,13 @@
 package com.example.projekt;
 
+import java.util.List;
+
 public class Printers {
+    private List<Printer> printers;
+    private void printOrder(PrintOrders.PrintOrder order){}
+
     public static class Printer {
+        private int qualityOfPrint;
+        private int printerId;
     }
 }
